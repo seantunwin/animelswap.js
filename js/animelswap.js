8b914df9dfc66fe53007dc3d11d90e57fbd5edb0
Sean T. Unwin <sean.t.unwin@gmail.com>
2013-06-09 09:33:18 -0400

    edited js and updated readme to reflect
    : added dir variable to make settings easier to manage

--- js/animelswap.js
+++ js/animelswap.js
@@ -1,24 +1,30 @@
 $(document).ready(function() {
   var el1 = $('.hldr').children(':first-child');
   var el2 = $('.hldr').children(':last-child');
-  var pos1 = el1.position().top;
-  var pos2 = el2.position().top;
+  var dir = 'top';
+  var pos1 = el1.offset()[dir];
+  var pos2 = el2.offset()[dir];
   var swapped = false;
   var animComplete = true;
   
   function swap() {
     if (animComplete == false) { return; }
     
-    var endY = Math.round(pos2 - pos1);
+    var endPt = Math.round(pos2 - pos1);
     var a = (swapped == false) ? el1 : el2;
     var b = (swapped == false) ? el2 : el1;
-
+    var aprop = {};
+    var bprop = {};
+    
     animComplete = false;
+    bprop[dir] = '-=' + endPt;
+    endPt = (swapped == true) ? (endPt - endPt) : endPt;
+    aprop[dir] = endPt;
+
     b.animate({opacity: 0}, 500);
-    b.animate({top: '-=' + endY}, 0);
-    endY = (swapped == true) ? (endY - endY) : endY;
+    b.animate(bprop, 0);
     a.css({'z-index': '1'})
-    a.animate({top: endY}, 
+    a.animate(aprop, 
                   {duration: 800,
                    complete: function() {
                      animComplete = true;
